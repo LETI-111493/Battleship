23a2b4718b7b436ab921e78b2ee60c2855af2329
LETI-111493 <pfsal@iscte-iul.pt>
2025-11-13 14:23:51 +0000

    correçao de erros

--- src/test/java/iscteiul/ista/AppTest.java
+++ src/test/java/iscteiul/ista/AppTest.java
@@ -1,38 +1,18 @@
 package iscteiul.ista;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Unit test for simple App.
+ * Teste unitário básico para verificar o ambiente de testes do projeto Battleship.
  */
-public class AppTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
     /**
-     * @return the suite of tests being tested
+     * Teste simples apenas para confirmar que o ambiente de testes está a funcionar.
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    void testAppRuns() {
+        assertTrue(true, "O teste básico passou com sucesso!");
     }
 }
